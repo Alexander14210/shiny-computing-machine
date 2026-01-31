d868db0b72edbadcfcf846719f8489ae0f3cebae
Alejandro <santamariaalejandro232@gmail.com>
2026-01-31 15:58:13 -0500

    change to raw file the github link

--- build/10-build.sh
+++ build/10-build.sh
@@ -47,7 +47,7 @@ echo "::group:: Install Packages"
 # Example: dnf5 install -y tmux
 
 dnf5 install -y git nodejs
-dnf5 install -y https://github.com/coldsprinkles/three-finger-libinput-gnome-patch/blob/main/libinput-1.30.1-1.fc43.x86_64.rpm
+dnf5 install -y https://github.com/coldsprinkles/three-finger-libinput-gnome-patch/raw/refs/heads/main/libinput-1.30.1-1.fc43.x86_64.rpm
 
 # Example using COPR with isolated pattern:
 # copr_install_isolated "ublue-os/staging" package-name
